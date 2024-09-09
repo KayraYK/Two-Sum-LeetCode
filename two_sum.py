9a478427a0115ef68b1c310d0ef4ad54cf7cc922
Kayra Kazanci <117873025+KayraYK@users.noreply.github.com>
2024-09-09 16:06:12 -0400

    Start of two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -5,3 +5,11 @@ two_sum.py
   Author(s): Kayra Kazanci
 '''
 
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
